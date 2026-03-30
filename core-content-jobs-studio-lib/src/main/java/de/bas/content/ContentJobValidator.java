45ab898603a6537c07f8d1859ef4b050a60186cf
Markus Schwarz <mschwarz@black-app-solutions.de>
2026-03-30 10:37:51 +0200

    Cleanup

--- core-content-jobs-studio-lib/src/main/java/de/bas/content/ContentJobValidator.java
+++ core-content-jobs-studio-lib/src/main/java/de/bas/content/ContentJobValidator.java
@@ -1,28 +1,45 @@
 package de.bas.content;
 
-import com.coremedia.cap.content.Content;
+import com.coremedia.cap.common.CapObject;
+import com.coremedia.cap.common.CapType;
+import com.coremedia.cap.content.ContentRepository;
 import com.coremedia.cap.struct.Struct;
-import com.coremedia.rest.cap.validation.ContentTypeValidatorBase;
+import com.coremedia.rest.cap.validation.CapTypeValidator;
 import com.coremedia.rest.validation.Issues;
 import com.coremedia.rest.validation.Severity;
-import org.springframework.util.StringUtils;
+import org.springframework.util.ObjectUtils;
 
 /**
  * @author Markus Schwarz
  */
-public class ContentJobValidator extends ContentTypeValidatorBase {
+public class ContentJobValidator implements CapTypeValidator {
     // Intentionally not used de.bas.contentsync.beans.ContentSync.* (as long as the contentbeans are
     // not in a separate lib) to avoid including the de.bas.contentsync.engine in a second component.
     protected static final String JOB_TYPE = "job-type";
     protected static final String LOCAL_SETTINGS = "localSettings";
+    private final ContentRepository contentRepository;
+
+    public ContentJobValidator(ContentRepository contentRepository) {
+        this.contentRepository = contentRepository;
+    }
 
     @Override
-    public void validate(Content content, Issues issues) {
+    public CapType getType() {
+        return contentRepository.getType("ContentJob");
+    }
+
+    @Override
+    public boolean isValidatingSubtypes() {
+        return false;
+    }
+
+    @Override
+    public void validate(CapObject content, Issues issues) {
         Struct localSettings = content.getStruct(LOCAL_SETTINGS);
 
         // Validate form fields
         if (localSettings != null) {
-            if (StringUtils.isEmpty(localSettings.getString(JOB_TYPE))) {
+            if (ObjectUtils.isEmpty(localSettings.getString(JOB_TYPE))) {
                 // content-sync-studio-plugin/src/main/joo/de/bas/contentsync/studio/bundles/FormValidation.properties
                 // ---------------------------------------------------------------------------------------------------
                 // Validator_content_sync_type_not_set_text=..
